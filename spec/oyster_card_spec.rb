310db446511e17c0b95a12a0361a243cb7d5cc7f
joshuaabrookuk <joshuabrookuk@gmail.com>
2020-10-20 16:26:47 +0100

    RED: Oystercard is expected to respond to #touch_out

--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -53,4 +53,14 @@ describe Oystercard do
       expect(subject.in_journey?).to eq true
     end
   end
+
+  it { should respond_to :touch_out }
+
+  # describe '#touch_out' do
+  #   it 'should set #in_journey? to false' do
+  #     subject.touch_in
+  #     subject.touch_out
+  #     expect(subject.in_journey?).to eq false
+  #   end
+  # end
 end
