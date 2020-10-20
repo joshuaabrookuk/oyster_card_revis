9b47e67002902b5a895629db4eed8bf784932d1c
joshuaabrookuk <joshuabrookuk@gmail.com>
2020-10-20 15:25:13 +0100

    RED: Oystercard #deduct should deduct the ammout from balance

--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -25,4 +25,11 @@ describe Oystercard do
 
   it { should respond_to :deduct }
 
+  describe '#deduct' do
+    it 'should deduct the ammout from balance' do
+      subject.top_up(20)
+      subject.deduct(15)
+      expect(subject.balance).to eq 15
+    end
+  end
 end
