8a6bc936f4e30b78dddf164be74168fcd8616a18
joshuaabrookuk <joshuabrookuk@gmail.com>
2020-10-20 11:57:15 +0100

    RED: Oystercard.top_up(ammount) should add ammount to #balance

--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -6,4 +6,11 @@ describe Oystercard do
   it { should respond_to :balance }
 
   it { should respond_to(:top_up).with(1).arguments }
+
+  describe '#top_up' do
+    it 'should add the ammount to #balance' do
+      subject.top_up(20)
+      expect(subject.balance).to eq 20
+    end
+  end 
 end
