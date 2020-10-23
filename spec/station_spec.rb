b7afce1de5b521036809d63841ec19da9ccffb77
joshuaabrookuk <joshuabrookuk@gmail.com>
2020-10-23 11:35:36 +0100

    RED: Station initialize when given a name parameter takes the name it is given

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -4,6 +4,15 @@ require 'station'
 
 describe Station do
 
+  describe 'initialize' do
+    context 'when given a name parameter' do
+      it 'takes the name it is given' do
+        subject('Waterloo')
+        expect(subject.name).to eq 'Waterloo'
+      end
+    end
+  end
+
   it { should respond_to :name }
 
   it { should respond_to :zone }
