a674719081fff7140c86a160683816af4843fff5
joshuaabrookuk <joshuabrookuk@gmail.com>
2020-10-23 10:49:14 +0100

    RED: Station #initialize should initialize with an empty name

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -3,5 +3,12 @@
 require 'station'
 
 describe Station do
+
+  describe '#initialize' do
+    it 'should initialize with an empty name' do
+      expect(subject.name).to eq ""
+    end
+  end
+
   it { should respond_to :name }
 end
