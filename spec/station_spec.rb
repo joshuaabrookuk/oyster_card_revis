6f159927ec38b94ed89a3ed408c0bdb886acc003
joshuaabrookuk <joshuabrookuk@gmail.com>
2020-10-23 16:20:44 +0100

    GREEN!!!!!! Now I get it

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -4,11 +4,17 @@ require 'station'
 
 describe Station do
 
+  let (:subject) {Station.new('Waterloo',1)}
+
   describe 'initialize' do
     context 'when given a name parameter' do
+      
       it 'takes the name it is given' do
-        subject('Waterloo')
         expect(subject.name).to eq 'Waterloo'
+      end
+
+      it 'takes the zone it is given' do
+        expect(subject.zone).to eq 1
       end
     end
   end
@@ -16,4 +22,5 @@ describe Station do
   it { should respond_to :name }
 
   it { should respond_to :zone }
+
 end
