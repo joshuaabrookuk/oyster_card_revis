0771c6ca3210ded2ce5fb482b912f4cb757f5fb5
joshuaabrookuk <joshuabrookuk@gmail.com>
2020-10-23 16:23:36 +0100

    REFACTOR:Syntax clean up

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -3,16 +3,13 @@
 require 'station'
 
 describe Station do
-
-  let (:subject) {Station.new('Waterloo',1)}
+  let(:subject) { Station.new('Waterloo', 1) }
 
   describe 'initialize' do
     context 'when given a name parameter' do
-      
       it 'takes the name it is given' do
         expect(subject.name).to eq 'Waterloo'
       end
-
       it 'takes the zone it is given' do
         expect(subject.zone).to eq 1
       end
@@ -22,5 +19,4 @@ describe Station do
   it { should respond_to :name }
 
   it { should respond_to :zone }
-
 end
