9c6a0d827e7ade30c47460a12353264f864bf72d
joshuaabrookuk <joshuabrookuk@gmail.com>
2020-10-23 11:01:13 +0100

    RED: Station is expected to respond to #zone and initialize with 0

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -8,7 +8,13 @@ describe Station do
     it 'should initialize with an empty name' do
       expect(subject.name).to eq ""
     end
+
+    it 'should initialize with zone set to 0' do
+      expect(subject.zone).to eq 0
+    end
   end
 
   it { should respond_to :name }
+
+  it { should respond_to :zone }
 end
